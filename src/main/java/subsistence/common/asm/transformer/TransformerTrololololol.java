0db633f6ec7a48bab1dcd4a7a7092220b36cf68d
dmillerw <dmillerw@gmail.com>
2015-05-16 23:38:22 -0700

    Less invasive troll

--- src/main/java/subsistence/common/asm/transformer/TransformerTrololololol.java
+++ src/main/java/subsistence/common/asm/transformer/TransformerTrololololol.java
@@ -2,15 +2,18 @@ package subsistence.common.asm.transformer;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
+
+import java.util.Iterator;
 
 /**
  * @author dmillerw
  */
 public class TransformerTrololololol extends CoreTransformer {
+
+    public TransformerTrololololol() {
+        mappings.put("getDisplayName", "func_96678_d");
+    }
 
     @Override
     public String getApplicableClass() {
@@ -23,11 +26,24 @@ public class TransformerTrololololol extends CoreTransformer {
         ClassNode classNode = new ClassNode();
         classReader.accept(classNode, 0);
 
+        boolean replace = false;
+
         // Modify findPlayerToAttack
         for (MethodNode methodNode : classNode.methods) {
             if (methodNode.name.equals("onPlayerJoinServer")) {
-                methodNode.instructions.clear();
-                methodNode.instructions.add(new InsnNode(Opcodes.RETURN));
+                Iterator<AbstractInsnNode> nodes = methodNode.instructions.iterator();
+                while (nodes.hasNext()) {
+                    AbstractInsnNode node = nodes.next();
+
+                    if (node instanceof MethodInsnNode)
+                        if ("getDisplayName".equals(getMappedName(((MethodInsnNode) node).name))) {
+                            replace = true;
+                        }
+                    if (node instanceof LdcInsnNode && replace) {
+                        ((LdcInsnNode) node).cst = "MattDahEpic";
+                        replace = false;
+                    }
+                }
             }
         }
 
