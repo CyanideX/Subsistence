1dec7a05cbda930d02352a393f9d0370f1fd3423
MattDahEpic <mattdahepic@gmail.com>
2015-05-16 21:52:36 -0600

    fix insults

--- src/main/java/subsistence/Subsistence.java
+++ src/main/java/subsistence/Subsistence.java
@@ -1,5 +1,6 @@
 package subsistence;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -8,6 +9,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 import subsistence.common.command.CommandSubsistence;
 import subsistence.common.command.CommandTPX;
@@ -37,6 +39,7 @@ public class Subsistence {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        FMLCommonHandler.instance().bus().register(instance);
         PacketHandler.initPackets();
         configPath = event.getModConfigurationDirectory().getPath() + File.separator + "/Subsistence/";
         proxy.preInit();
@@ -59,24 +62,28 @@ public class Subsistence {
 
     }
     @SubscribeEvent
-    public void playerLoggedIn (PlayerEvent.PlayerLoggedInEvent event) {
-        //just for you dylan  ^
-        if (event.player.getUniqueID().equals("54972f65-b920-4a60-b539-dc443c16cd29")) { //CyanideX
-            event.player.addChatMessage(new ChatComponentText("<CyanideX> I'm not so sure that text is such a bad idea Matt."));
-        } else if (event.player.getUniqueID().equals("315f746e-8e38-44c5-b604-fd4b7014ec8c")) { //Yulfie
-            event.player.addChatMessage(new ChatComponentText("<CyanideX> It's Yulife\n" +
-                    "<Inap> no\n" +
-                    "<CyanideX> Yeah it is"));
-        } else if (event.player.getUniqueID().equals("c715991d-e69c-48f9-a92d-8fc60c0829fb")) { //MattDahEpic
-            event.player.addChatComponentMessage(new ChatComponentText("Matt is so great. G. R. A. T."));
-        } else if (event.player.getUniqueID().equals("f579a27a-d06b-46a7-9f90-f77af54a108c")) { //dmillerw
-            event.player.addChatComponentMessage(new ChatComponentText("public static void parseMainSettings (File file)"));
-        } else if (event.player.getUniqueID().equals("dc697d82-dc39-4e11-814b-0a788f592842")) { //BuhneBatVampire
-            event.player.addChatComponentMessage(new ChatComponentText("You have played 72 hours of osu! today."));
-        } else if (event.player.getUniqueID().equals("f0f76db6-0461-4151-8ba7-392d65d62ea3")) { //lclc98
-            event.player.addChatComponentMessage(new ChatComponentText("What does func_149851_a do?"));
-        } else if (event.player.getUniqueID().equals("e43e9766-f903-48e1-818f-d41bb48d80d5")) { //FireBall1725
-            event.player.addChatComponentMessage(new ChatComponentText("Given [Fire Charge] * 1725 to FireBall1725"));
+    public void onPlayerJoinServer (PlayerEvent.PlayerLoggedInEvent event) {
+        //    just for you dylan  ^
+        if (!event.player.worldObj.isRemote) {
+            if (event.player instanceof EntityPlayerMP) {
+                if (event.player.getDisplayName().equalsIgnoreCase("CyanideX")) {
+                    event.player.addChatMessage(new ChatComponentText("<CyanideX> I'm not so sure that text is such a bad idea Matt."));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("Yulife")) {
+                    event.player.addChatMessage(new ChatComponentText("<CyanideX> It's Yulife\n" +
+                            "<Inap> no\n" +
+                            "<CyanideX> Yeah it is"));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("MattDahEpic")) {
+                    event.player.addChatComponentMessage(new ChatComponentText("Matt is so great. G. R. A. T."));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("dmillerw")) {
+                    event.player.addChatComponentMessage(new ChatComponentText("public static void parseMainSettings (File file)"));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("BuhneBatVampire")) {
+                    event.player.addChatComponentMessage(new ChatComponentText("You have played 72 hours of osu! today."));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("lclc98")) {
+                    event.player.addChatComponentMessage(new ChatComponentText("What does func_149851_a do?"));
+                } else if (event.player.getDisplayName().equalsIgnoreCase("FireBall1725")) {
+                    event.player.addChatComponentMessage(new ChatComponentText("Given [Fire Charge] * 1725 to FireBall1725"));
+                }
+            }
         }
     }
 }
