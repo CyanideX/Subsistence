5ddc38e1f4b2b9464e58c9aa9122c1d0d1ce9dd5
MattDahEpic <mattdahepic@gmail.com>
2015-05-19 19:07:58 -0600

    added metal press spam delay

--- src/main/java/subsistence/common/tile/machine/TileMetalPress.java
+++ src/main/java/subsistence/common/tile/machine/TileMetalPress.java
@@ -19,13 +19,19 @@ public class TileMetalPress extends TileCoreMachine {
     public ItemStack itemStack;
     @NBTHandler.Sync(false)
     public int amount;
+    @NBTHandler.Sync(false)
+    public int pauseCount;
 
     @Override
     public void updateEntity() {
         //if animations were to go here, they would spam. put them in activate
         if (!worldObj.isRemote) {
-            if (itemStack == null)
+            if (itemStack == null) {
                 amount = 0;
+                pauseCount = 0;
+            } else if (pauseCount < 40) {
+                pauseCount++;
+            }
         }
     }
 
@@ -33,16 +39,18 @@ public class TileMetalPress extends TileCoreMachine {
         if (itemStack == null)
             return;
 
-        amount++;
-
         MetalPressRecipe recipe = SubsistenceRecipes.METAL_PRESS.get(itemStack);
-        if (recipe != null) {
+        if (recipe != null && pauseCount >= 40) {
+            amount++;
             SubsistenceLogger.info("*clang*"); //TODO: remove and replace with animations @dmillerw
+            pauseCount = 0;
             if (amount >= recipe.getAmount()) {
                 itemStack = recipe.getOutputItem();
                 SubsistenceLogger.info("Your hard work has earned you a "+itemStack.getDisplayName());
                 worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
             }
+        } else {
+            SubsistenceLogger.info("Recipe not valid or animation not finished. No clang for you.");
         }
     }
 }
