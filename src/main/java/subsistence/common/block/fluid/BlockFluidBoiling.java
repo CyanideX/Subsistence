4f2e32050daa1508c0293e76d45744a68d68c44e
dmillerw <dmillerw@gmail.com>
2015-05-22 15:30:06 -0700

    Boiling water turns to netherrack when touching lava in the nether

--- src/main/java/subsistence/common/block/fluid/BlockFluidBoiling.java
+++ src/main/java/subsistence/common/block/fluid/BlockFluidBoiling.java
@@ -3,9 +3,13 @@ package subsistence.common.block.fluid;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialLiquid;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
@@ -21,10 +25,12 @@ import java.util.Random;
 
 public class BlockFluidBoiling extends SubsistenceBasicFluid {
 
+    private static final Material water_boiling = (new MaterialLiquid(MapColor.waterColor));
+
     EnumParticle particleBubble;
 
     public BlockFluidBoiling(Fluid fluid) {
-        super(fluid, Material.water);
+        super(fluid, BlockFluidBoiling.water_boiling);
         setQuantaPerBlock(4);
         particleBubble = EnumParticle.BUBBLE;
     }
@@ -67,6 +73,63 @@ public class BlockFluidBoiling extends SubsistenceBasicFluid {
                     if (randSound > 0.8D) {
                         world.playSoundEffect((int) b.xCoord, (int) b.yCoord, (int) b.zCoord, "random.fizz", 0.05f, 0.0f);
                     }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z) {
+        super.onBlockAdded(world, x, y, z);
+        tryTransform(world, x, y, z);
+    }
+
+    @Override
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+        super.onNeighborBlockChange(world, x, y, z, block);
+        tryTransform(world, x, y, z);
+    }
+
+    private void tryTransform(World world, int x, int y, int z) {
+        if (world.getBlock(x, y, z) == this) {
+            boolean flag = false;
+
+            if (flag || world.getBlock(x, y, z - 1).getMaterial() == Material.lava) {
+                flag = true;
+            }
+
+            if (flag || world.getBlock(x, y, z + 1).getMaterial() == Material.lava) {
+                flag = true;
+            }
+
+            if (flag || world.getBlock(x - 1, y, z).getMaterial() == Material.lava) {
+                flag = true;
+            }
+
+            if (flag || world.getBlock(x + 1, y, z).getMaterial() == Material.lava) {
+                flag = true;
+            }
+
+            if (flag || world.getBlock(x, y + 1, z).getMaterial() == Material.lava) {
+                flag = true;
+            }
+
+            if (flag) {
+                if (world.provider.dimensionId == -1) {
+                    world.setBlock(x, y, z, Blocks.netherrack);
+                } else {
+                    int metadata = world.getBlockMetadata(x, y, z);
+
+                    if (metadata == 0) {
+                        world.setBlock(x, y, z, Blocks.obsidian);
+                    } else if (metadata <= 4) {
+                        world.setBlock(x, y, z, Blocks.cobblestone);
+                    }
+                }
+
+                world.playSoundEffect((double) ((float) x + 0.5F), (double) ((float) y + 0.5F), (double) ((float) z + 0.5F), "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+                for (int l = 0; l < 8; ++l) {
+                    world.spawnParticle("largesmoke", (double) x + Math.random(), (double) y + 1.2D, (double) z + Math.random(), 0.0D, 0.0D, 0.0D);
                 }
             }
         }
