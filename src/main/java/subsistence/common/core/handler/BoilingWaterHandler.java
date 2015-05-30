1585004c7736bbbdf59bfaec2c884a13e23dc722
dmillerw <dmillerw@gmail.com>
2015-05-29 19:03:37 -0700

    Reference dimension ID, not save file

--- src/main/java/subsistence/common/core/handler/BoilingWaterHandler.java
+++ src/main/java/subsistence/common/core/handler/BoilingWaterHandler.java
@@ -1,24 +1,20 @@
 package subsistence.common.core.handler;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
-import subsistence.Subsistence;
 import subsistence.common.item.SubsistenceItems;
 
 public class BoilingWaterHandler {
 
     @SubscribeEvent
     public void onEnterNether(EntityJoinWorldEvent event) {
-        if (!event.world.getWorldInfo().getWorldName().equalsIgnoreCase("DIM-1")) {
+        if (event.world.provider.dimensionId != -1) {
             return;
         }
 
