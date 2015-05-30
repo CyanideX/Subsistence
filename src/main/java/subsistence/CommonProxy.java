ed1c3b549c39a9129bd47c5d14a4f5590d83dc43
dmillerw <dmillerw@gmail.com>
2015-05-29 19:18:18 -0700

    Cleanup

--- src/main/java/subsistence/CommonProxy.java
+++ src/main/java/subsistence/CommonProxy.java
@@ -1,18 +1,11 @@
 package subsistence;
 
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.GameData;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
 import subsistence.common.block.SubsistenceBlocks;
 import subsistence.common.config.ConfigManager;
 import subsistence.common.core.handler.*;
 import subsistence.common.fluid.SubsistenceFluids;
-import subsistence.common.item.ItemHammer;
 import subsistence.common.item.SubsistenceItems;
-import subsistence.common.lib.tool.ToolDefinition;
 import subsistence.common.network.nbt.data.AbstractSerializer;
 import subsistence.common.recipe.SubsistenceRecipes;
 import subsistence.common.util.EventUtil;
@@ -37,26 +30,10 @@ public class CommonProxy {
     }
 
     public void init() {
+
     }
 
     public void postInit() {
-
         ConfigManager.loadAllFiles();
-
-        // Tool registration
-        ToolDefinition.register(new ItemStack(SubsistenceItems.hammerWood), ToolDefinition.HAMMER, ItemHammer.STRENGTH[0]);
-        ToolDefinition.register(new ItemStack(SubsistenceItems.hammerStone), ToolDefinition.HAMMER, ItemHammer.STRENGTH[1]);
-        ToolDefinition.register(new ItemStack(SubsistenceItems.hammerIron), ToolDefinition.HAMMER, ItemHammer.STRENGTH[2]);
-        ToolDefinition.register(new ItemStack(SubsistenceItems.hammerSteel), ToolDefinition.HAMMER, ItemHammer.STRENGTH[3]);
-        ToolDefinition.register(new ItemStack(SubsistenceItems.hammerDiamond), ToolDefinition.HAMMER, ItemHammer.STRENGTH[4]);
-
-        // Axe registration
-        for (Object key : GameData.getItemRegistry().getKeys()) {
-            Item axe = (Item) Item.itemRegistry.getObject(key.toString());
-
-            if (axe instanceof ItemAxe) {
-                ToolDefinition.register(new ItemStack(axe), ToolDefinition.AXE, ((ItemTool) axe).func_150913_i().getEfficiencyOnProperMaterial());
-            }
-        }
     }
 }
