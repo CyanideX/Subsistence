36072c9f0e0f22d0623ac7668bd26d2994cbbcad
dmillerw <dmillerw@gmail.com>
2015-06-02 12:25:42 -0700

    Use PlayerChangedDimensionEvent

--- src/main/java/subsistence/common/core/handler/BoilingWaterHandler.java
+++ src/main/java/subsistence/common/core/handler/BoilingWaterHandler.java
@@ -1,11 +1,11 @@
 package subsistence.common.core.handler;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -15,23 +15,21 @@ import subsistence.common.fluid.SubsistenceFluids;
 public class BoilingWaterHandler {
 
     @SubscribeEvent
-    public void onEnterNether(EntityJoinWorldEvent event) {
-        if (event.world.provider.dimensionId != -1) {
+    public void onEnterNether(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if (event.toDim != -1) {
             return;
         }
 
-        if (event.entity instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer)event.entity;
-            InventoryPlayer inventoryPlayer = player.inventory;
-            for (int i=0; i < inventoryPlayer.getSizeInventory(); i++) {
-                ItemStack item = inventoryPlayer.getStackInSlot(i);
-                if (item != null) {
-                    FluidStack fluidStack = FluidContainerRegistry.getFluidForFilledItem(item);
-                    if (fluidStack != null && fluidStack.getFluid() == FluidRegistry.WATER) {
-                        ItemStack drained = FluidContainerRegistry.drainFluidContainer(item);
-                        ItemStack filled = FluidContainerRegistry.fillFluidContainer(new FluidStack(SubsistenceFluids.boilingWaterFluid, fluidStack.amount), drained);
-                        inventoryPlayer.setInventorySlotContents(0, filled);
-                    }
+        EntityPlayer player = event.player;
+        InventoryPlayer inventoryPlayer = player.inventory;
+        for (int i=0; i < inventoryPlayer.getSizeInventory(); i++) {
+            ItemStack item = inventoryPlayer.getStackInSlot(i);
+            if (item != null) {
+                FluidStack fluidStack = FluidContainerRegistry.getFluidForFilledItem(item);
+                if (fluidStack != null && fluidStack.getFluid() == FluidRegistry.WATER) {
+                    ItemStack drained = FluidContainerRegistry.drainFluidContainer(item);
+                    ItemStack filled = FluidContainerRegistry.fillFluidContainer(new FluidStack(SubsistenceFluids.boilingWaterFluid, fluidStack.amount), drained);
+                    inventoryPlayer.setInventorySlotContents(0, filled);
                 }
             }
         }
