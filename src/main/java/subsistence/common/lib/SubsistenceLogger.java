6a5130ea779c1dcaf7eaf873c334d98f5125fb8f
tterrag1098 <tterrag1098@gmail.com>
2015-06-03 00:28:15 -0400

    Clean up logging

--- src/main/java/subsistence/common/lib/SubsistenceLogger.java
+++ src/main/java/subsistence/common/lib/SubsistenceLogger.java
@@ -1,44 +1,48 @@
 package subsistence.common.lib;
 
-import cpw.mods.fml.common.FMLLog;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import subsistence.Subsistence;
 
 public class SubsistenceLogger {
+    
+    private static final Logger log = LogManager.getLogger(Subsistence.NAME);
 
-    public static void log(Level logLevel, Object object) {
-        FMLLog.log(Subsistence.NAME, logLevel, String.valueOf(object));
+    public static void log(Level level, Object object, Object... args) {
+        log.log(level, String.valueOf(object), args);
     }
 
-    public static void all(Object object) {
+    public static void all(Object object, Object... args) {
         log(Level.ALL, object);
     }
 
-    public static void debug(Object object) {
-        log(Level.DEBUG, object);
+    public static void debug(Object object, Object... args) {
+        log(Level.DEBUG, object, args);
     }
 
-    public static void error(Object object) {
-        log(Level.ERROR, object);
+    public static void error(Object object, Object... args) {
+        log(Level.ERROR, object, args);
     }
 
-    public static void fatal(Object object) {
-        log(Level.FATAL, object);
+    public static void fatal(Object object, Object... args) {
+        log(Level.FATAL, object, args);
     }
 
-    public static void info(Object object) {
-        log(Level.INFO, object);
+    public static void info(Object object, Object... args) {
+        log(Level.INFO, object, args);
     }
 
-    public static void off(Object object) {
-        log(Level.OFF, object);
+    public static void off(Object object, Object... args) {
+        log(Level.OFF, object, args);
     }
 
-    public static void trace(Object object) {
-        log(Level.TRACE, object);
+    public static void trace(Object object, Object... args) {
+        log(Level.TRACE, object, args);
     }
 
-    public static void warn(Object object) {
-        log(Level.WARN, object);
+    public static void warn(Object object, Object... args) {
+        log(Level.WARN, object, args);
     }
 }
