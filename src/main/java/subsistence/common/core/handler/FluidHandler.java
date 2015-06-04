2dd450a460e2492773a1bc8b6ec0f8b882181131
tterrag1098 <tterrag1098@gmail.com>
2015-06-04 13:24:45 -0400

    Fix boiling water not able to be picked up without CoFHCore

--- src/main/java/subsistence/common/core/handler/FluidHandler.java
+++ src/main/java/subsistence/common/core/handler/FluidHandler.java
@@ -1,16 +1,16 @@
 package subsistence.common.core.handler;
 
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import subsistence.common.block.SubsistenceBlocks;
 import subsistence.common.fluid.SubsistenceFluids;
-import subsistence.common.item.ItemWoodenBucket;
 import subsistence.common.item.SubsistenceItems;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * @author dmillerw
@@ -30,18 +30,18 @@ public class FluidHandler {
         return true;
     }
 
-//    @SubscribeEvent
-//    public void onFillBucket(FillBucketEvent event) {
-//        Block block = event.world.getBlock(event.target.blockX, event.target.blockY, event.target.blockZ);
-//        int meta = event.world.getBlockMetadata(event.target.blockX, event.target.blockY, event.target.blockZ);
-//        if (block == SubsistenceBlocks.boilingWater && meta == 0) {
-//            if (event.current.getItem() == Items.bucket) {
-//                event.world.setBlockToAir(event.target.blockX, event.target.blockY, event.target.blockZ);
-//                event.result = new ItemStack(SubsistenceItems.boilingBucket);
-//                event.setResult(Event.Result.ALLOW);
-//            }
-//        }
-//    }
+    @SubscribeEvent
+    public void onFillBucket(FillBucketEvent event) {
+        Block block = event.world.getBlock(event.target.blockX, event.target.blockY, event.target.blockZ);
+        int meta = event.world.getBlockMetadata(event.target.blockX, event.target.blockY, event.target.blockZ);
+        if (block == SubsistenceBlocks.boilingWater && meta == 0) {
+            if (event.current.getItem() == Items.bucket) {
+                event.world.setBlockToAir(event.target.blockX, event.target.blockY, event.target.blockZ);
+                event.result = new ItemStack(SubsistenceItems.boilingBucket);
+                event.setResult(Event.Result.ALLOW);
+            }
+        }
+    }
 //
 //    @SubscribeEvent
 //    public void onFluidRegistered(FluidRegistry.FluidRegisterEvent event) {
